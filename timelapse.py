84c4af2b2aa7b0f6b3b5eca56546fb594b9bc750
bcrowell <brettcrowell@gmail.com>
2016-09-11 11:12:10 -0400

    moving gphoto2 specific code into subclass

--- timelapse.py
+++ timelapse.py
@@ -1,5 +1,4 @@
 import time
-import gphoto2 as gp
 import os
 import threading
 import signal
@@ -32,65 +31,7 @@ class Timelapse:
         return None
 
     def take_picture(self, image):
-
-        image_filename = "{}-{}".format(image['name'], image['ts'])
-
-        print("Capturing image `{}`".format(image_filename))
-
-        context = gp.gp_context_new()
-        camera = gp.check_result(gp.gp_camera_new())
-        target = None
-
-        try:
-
-            # initialize the camera
-            gp.check_result(gp.gp_camera_init(camera, context))
-
-            # save information about found camera
-            # print(gp.check_result(gp.gp_camera_get_summary(camera, context)))
-
-            # capture image, making note of the file path on memory card
-            file_path = gp.check_result(gp.gp_camera_capture(camera, gp.GP_CAPTURE_IMAGE, context))
-
-            print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-
-            # get reference to image file on camera
-            camera_file = gp.check_result(gp.gp_camera_file_get(camera,
-                                                                file_path.folder,
-                                                                file_path.name,
-                                                                gp.GP_FILE_TYPE_NORMAL,
-                                                                context))
-
-            # determine where to store this file locally
-            target = os.path.join(self.preferences['location'], "output", "{}.jpg".format(image_filename))
-
-            print('Copying image to', target)
-
-            # download image to directory determined above
-            gp.check_result(gp.gp_file_save(camera_file, target))
-
-            # @todo need to check file size here
-
-        except gp.GPhoto2Error as ex:
-
-            if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
-                print("Unable to find usable camera.  Is it connected, alive, and awake? ({})".format(ex.string))
-
-            elif ex.code == gp.GP_ERROR_IO_USB_CLAIM:
-                print("Camera is already in use.  If on Mac, try running `sudo killall PTPCamera` ({})".format(ex.string))
-
-            elif ex.code == gp.GP_ERROR_IO:
-                print ("I/O issue with camera, USB connection needs to be reset ({})".format(ex.string))
-
-            else:
-                print("GPhoto2 Error: {}".format(ex.string))
-
-        finally:
-
-            # let go of the camera now
-            gp.check_result(gp.gp_camera_exit(camera, context))
-
-        return target
+        raise NotImplementedError
 
     def upload_to_s3(self, image_path, image):
         return None
--- timelapse_server.py
+++ timelapse_server.py
@@ -1,9 +1,9 @@
 import json
-from timelapse import Timelapse
+from gphoto2_timelapse import GPhoto2Timelapse
 from sequence import Sequence
 
 with open("data.json") as data_file:
     exposures = json.load(data_file)
 
 seq = Sequence(exposures)
-lapse = Timelapse(seq)
+lapse = GPhoto2Timelapse(seq)
