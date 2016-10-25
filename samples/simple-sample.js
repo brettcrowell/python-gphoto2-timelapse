167d36fcb62f4746c4eea4b2dba9cc9f0b67f72a
bcrowell <brettcrowell@gmail.com>
2016-10-25 00:28:20 -0400

    making simple sample even simpler

--- samples/simple-sample.js
+++ samples/simple-sample.js
@@ -5,10 +5,18 @@ var exposures = [];
 // 24 hour timelapse = (24*60*60) / (60*30)
 // ie: (24h * 60m * 60s) / (60s * 30fps)
 
-for(var i = 0; i < 1800; i++){
+var delaySeconds = 30;
+var inputHours = 24;
+var outputSeconds = 60;
+var outputFps = 30;
+
+var intervalSeconds = (inputHours * 60 * 60) / (outputSeconds * outputFps);
+var numFrames = outputSeconds * outputFps;
+
+for(var i = 0; i < numFrames; i++){
   exposures.push({
-    "name": "eight-hour-test",
-    "ts": epoch + (i * 48000)
+    "name": "twenty-four-hour-test",
+    "ts": (delaySeconds * 1000) + epoch + (i * (intervalSeconds * 1000))
   })
 }
 
