1422c95d5577c54a414a7c95446ce6c9004591f4
Brett Crowell <brettcrowell@gmail.com>
2018-07-17 23:18:49 -0400

    read parameters from command line

--- timelapse_server.py
+++ timelapse_server.py
@@ -6,6 +6,8 @@ from sequence import Sequence
 from logger import Logger
 from timelapse_errors import TimelapseError
 from uploader import S3BucketUploader
+from argparse import ArgumentParser, FileType
+import time
 import os
 
 def save_state_to_disk(filename):
@@ -40,19 +42,88 @@ try:
 
     except (OSError, ValueError):
 
-        with open("data.json") as data_file:
+        parser = ArgumentParser()
 
-            # otherwise start a new one
-            exposures = json.load(data_file)
+        parser.add_argument(
+            '-s',
+            '--span',
+            help='The number of hours your timelapse will run for, starting now.',
+            default=24,
+            type=int
+        )
 
-            # make sure these are globals
-            logr = Logger()
-            seq = Sequence(exposures, logr)
-            uploader = S3BucketUploader()
+        parser.add_argument(
+            '-d',
+            '--duration',
+            help='The duration of the assembled timelapse, in seconds.',
+            default=60,
+            type=int
+        )
 
-            lapse = GPhoto2Timelapse(seq, uploader, logr)
+        parser.add_argument(
+            '-f',
+            '--fps',
+            help='The frame rate, in frames per second, of the assembled timelapse.',
+            default=30,
+            type=int
+        )
 
-            logr.log("New lapse started by `timelapse_server`")
+        parser.add_argument(
+            '-n',
+            '--name',
+            help='The name of the timelapse.  Also serves as the directory name for frames.',
+            default="timelapse",
+        )
+
+        parser.add_argument(
+            '-b',
+            '--bucket',
+            help='The name of the S3 Bucket where frames will be uploaded.',
+            default="bc-timelapse"
+        )
+
+        parser.add_argument(
+            '-i',
+            '--input',
+            help='The relative path of a JSON file containing an array of timestamps.  Overrides --span, --duration, and --fps',
+            default=None,
+            type=FileType("r")
+        )
+
+        args = parser.parse_args()
+
+        if args.input:
+
+            with args.input as data_file:
+
+                # otherwise start a new one
+                exposures = json.load(data_file)
+
+        else:
+
+            epoch = int(round(5000 + (time.time() * 1000)))
+
+            # calculate the interval between captures and the total #/frames
+            numFrames = args.duration * args.fps
+            intervalMilliseconds = round(
+                (args.span * 60 * 60 * 1000) / numFrames)
+
+            exposures = list(map(
+                (lambda i: dict([
+                    ("name", args.name),
+                    ("ts", epoch + (i * intervalMilliseconds))
+                ])),
+                range(numFrames)
+            ))
+
+        # make sure these are globals
+        logr = Logger()
+        seq = Sequence(exposures, logr)
+        uploader = S3BucketUploader()
+
+        lapse = GPhoto2Timelapse(seq, uploader, logr)
+
+        logr.log("New lapse started by `timelapse_server`")
 
     # and we're off!
     lapse.take_next_picture()
